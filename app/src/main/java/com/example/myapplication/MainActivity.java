ccb6632e3ed57c0e40a5434df4cb55eaa2ae5cca
91094638 <jayden.jimenez2005@gmail.com>
2022-08-30 12:18:24 -0700

    I added required images and started to create my toast but it wasn't acting right i may have to show the toast on an actual phone.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void next(View v)
     {
+        Log.i("click", "you clicked button next");
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
         startActivity(intent);
-
+        Toast.makeText(this, "you clicked next!", Toast.LENGTH_LONG).show();
     }
 
 }
